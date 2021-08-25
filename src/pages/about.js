48feaab47395abadcaf2a4867e2721882303210b
dhui <dhui0226@gmail.com>
2021-08-25 18:49:59 -0500

    refactored nav links

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,11 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/Layout"
 
 const AboutPage = () => {
     return (
-        <main>
-            <title>About Me</title>
-            <h1>About Me</h1>
+        <Layout pageTitle="About Page">
             <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-            <Link to="/">Home</Link>
-        </main>
+        </Layout>
     )
 }
 
